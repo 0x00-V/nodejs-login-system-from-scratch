b4e4e845c99736b56c5b7a5f9f206122e0a3cae7
Daniel Thomas <business.danielthomas@outlook.com>
2025-09-24 06:03:41 +0100

    init

---
+++ server.js
@@ -0,0 +1,121 @@
+import { createServer } from 'http';
+import crypto from 'crypto';
+import { readFile } from 'fs'; 
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+import { Client } from 'pg';
+
+const client = new Client({
+	user: 'dan',
+	password: process.env.DB_PASS,
+	host: '127.0.0.1',
+	port: 5432,
+	database: 'testdb',
+});
+
+
+
+await client.connect();
+const hostname = '127.0.0.1';
+const port = 8080;
+
+// Temporary Solution To Storing User Sessions
+const sessions = {};
+
+
+function parseCookies(cookieHeader)
+{
+	const cookies = {};
+	if (!cookieHeader) return cookies;
+	cookieHeader.split(';').forEach(cookie => {
+		const [name, ...rest] = cookie.trim().split('=');
+		cookies[name] = rest.join('=');
+	});
+	return cookies;
+};
+
+function generateSessionId()
+{
+	return crypto.randomBytes(16).toString('hex');
+}
+
+const server = createServer((req, res) => {
+	const cookies = parseCookies(req.headers.cookie);
+
+const url = req.url;
+
+if(req.url === '/login' && req.method === 'GET')
+{
+	readFile('./login.html', (err, data) => {
+		if(err)
+		{
+			res.writeHead(500, {'Content-Type': 'text/plain'});
+			res.end('Error loading page');
+		} else 
+		{
+			res.writeHead(200, {'Content-Type': 'text/html'});
+			res.end(data);
+		}
+	});
+} else if (url === '/login' && req.method === 'POST'){
+	let body = '';
+	req.on('data', chunk => (body += chunk));
+	req.on('end', async () => {
+	const params = new URLSearchParams(body);
+	const username = params.get('username');
+	const password = params.get('password');
+			
+	const text = "SELECT username, password, id FROM users WHERE username = $1 AND password = $2";
+	const q1_resp = await client.query(text, [username, password])
+	if(q1_resp.rows.length > 0)
+	{
+		console.log('Correct.');
+		const sessionId = generateSessionId();
+		sessions[sessionId] = {userId: q1_resp.rows[0].id, username: q1_resp.rows[0].username};
+		res.setHeader('Set-Cookie', `sessionId=${sessionId}; HttpOnly`);
+		res.writeHead(302, { Location: '/dashboard'});
+		res.end();
+	} else 
+	{
+		res.writeHead(401, {'Content-Type': 'text/plain'});
+		res.end('Incorrect Credentials.');
+	}
+	});
+} else if (req.url === '/logout')
+{
+	delete sessions[cookies.sessionId];
+	res.writeHead(302, { Location: '/'});
+	res.end(); 
+} else if (req.url === '/dashboard')
+{
+	const session = sessions[cookies.sessionId];
+	if(session)
+	{
+		readFile('./dashboard.html', 'utf8', (err, html) => {
+			if(err)
+			{
+				res.writeHead(500, {'Content-Type': 'text/plain'});
+				res.end('Error loading dashboard');
+			} else
+			{
+				const page = html.replace(`{{username}}`, session.username);
+				res.writeHead(200, {'Content-Type': 'text/html'});
+				res.end(page);
+			}
+		});
+	} else {
+		res.writeHead(302, { Location: '/'});
+		res.end();
+	}
+} else {
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('Index page. Try /login, /dashboard, or /logout.');
+}
+});
+
+server.listen(port, hostname, () => {
+	console.log(`Server running at http://${hostname}:${port}/`);
+});
